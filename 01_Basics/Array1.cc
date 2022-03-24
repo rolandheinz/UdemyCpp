2aab8333d95fe6baed9ab9c0a0bb27f9297d47f8
rolandheinz <66413618+rolandheinz@users.noreply.github.com>
2022-03-24 12:06:57 +0100

    test

---
+++ 01_Basics/Array1.cc
@@ -0,0 +1,12 @@
+
+#include <iostream>
+
+int main()
+{
+    //c-style array
+    int array1[10] = {10, 11, 12, 13, 14, 15, 16, 17, 18, 19};
+    std::cout << array1[6] << std::endl;
+
+
+    return 0;
+}
